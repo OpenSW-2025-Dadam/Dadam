b0ad6c14e9176e162de36eccd6317dab70f91d2f
s90d221 <sujin90d@gmail.com>
2025-11-07 15:08:22 +0900

    Initial project setup

---
+++ src/test/java/com/example/dadambackend/DadamBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dadambackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DadamBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
